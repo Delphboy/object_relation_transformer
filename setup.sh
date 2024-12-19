a036fc9eaf3e4342df73ed3958f1ad3b0453e896
Henry <hps1945@live.co.uk>
2024-12-19 16:41:43 +0000

    UPDATE: tweak hpc scripts and add setup script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+python3 -m venv .venv
+source .venv/bin/activate
+
+# PyTorch
+python3 -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+
+python3 -m pip install gensim
+python3 -m pip install matplotlib
+python3 -m pip install pandas
+python3 -m pip install scikit-image
+python3 -m pip install six
+python3 -m pip install typing
+python3 -m pip install requests
+python3 -m pip install h5py
+
