e93f0609380d5571a4adf178cea6f8de57bdb56e
Henry <hps1945@live.co.uk>
2024-12-21 19:08:45 +0000

    FIX: SCST training on andrena

--- models/__init__.py
+++ models/__init__.py
@@ -59,8 +59,6 @@ def setup(opt):
         assert os.path.isdir(opt.start_from)," %s must be a a path" % opt.start_from
         assert os.path.isfile(os.path.join(opt.start_from,"infos_"+opt.id+".pkl")),"infos.pkl file does not exist in path %s"%opt.start_from
         # model.load_state_dict(torch.load(os.path.join(opt.start_from, 'model.pth')))
-        # NOTE: Changed to load the best model
-        # TODO: model name should include opt.id
-        model.load_state_dict(torch.load(os.path.join(opt.start_from, 'model-best.pth')))
+        model.load_state_dict(torch.load(os.path.join(opt.start_from, 'model.pth')))
 
     return model
